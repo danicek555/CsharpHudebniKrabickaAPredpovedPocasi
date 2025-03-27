f06bb518ceefc87c9152ca8cbdd45752da650f8b
Daniel Mitka <danielmitka@Daniels-MacBook-Air.local>
2025-03-27 15:08:25 +0100

    Programky z pondeli 24.3

--- Program.cs
+++ Program.cs
@@ -16,18 +16,56 @@
 // Console.Beep(293, 500);
 
 
-int pondeli = Random.Shared.Next(-10, 40);
-int utery = pondeli + 3;
-int streda = utery - 2;
-int ctvrtek = streda + 5;
-int patek = ctvrtek - 4;
-int sobota = patek - 1;
-int nedele = sobota + 1;
+// int pondeli = Random.Shared.Next(-10, 40);
+// int utery = pondeli + 3;
+// int streda = utery - 2;
+// int ctvrtek = streda + 5;
+// int patek = ctvrtek - 4;
+// int sobota = patek - 1;
+// int nedele = sobota + 1;
 
-Console.WriteLine("Pondělí: " + pondeli);
-Console.WriteLine("Úterý: " + utery);
-Console.WriteLine("Středa: " + streda);
-Console.WriteLine("Čtvrtek: " + ctvrtek);
-Console.WriteLine("Pátek: " + patek);
-Console.WriteLine("Sobota: " + sobota);
-Console.WriteLine("Neděle: " + nedele);
+// Console.WriteLine("Pondělí: " + pondeli);
+// Console.WriteLine("Úterý: " + utery);
+// Console.WriteLine("Středa: " + streda);
+// Console.WriteLine("Čtvrtek: " + ctvrtek);
+// Console.WriteLine("Pátek: " + patek);
+// Console.WriteLine("Sobota: " + sobota);
+// Console.WriteLine("Neděle: " + nedele);
+
+
+// int nahodnyDen = Random.Shared.Next(1, 31);
+// int nahodnyRok = Random.Shared.Next(-10000, 2025);
+// int nahodnyMesic = Random.Shared.Next(1, 12);
+
+// int nahodnyDenMatky = Random.Shared.Next(1, 31);
+// int nahodnyMesicMatky = Random.Shared.Next(1, 12);
+// int nahodnyDenTatky = Random.Shared.Next(1, 31);
+// int nahodnyMesicTatky = Random.Shared.Next(1, 12);
+
+// int rokNaroMatky = nahodnyRok - 30;
+// int rokNaroTatky = nahodnyRok - 35;
+
+// Console.WriteLine("Narodil ses " + nahodnyDen + "." + nahodnyMesic + "." + nahodnyRok);
+// Console.WriteLine("Tvá mamča se narodila " + nahodnyDenMatky + "." + nahodnyMesicMatky + "." + rokNaroMatky);
+// Console.WriteLine("Tvůj táta se narodil " + nahodnyDenTatky + "." + nahodnyMesicTatky + "." + rokNaroTatky);
+
+Console.WriteLine("Zadej stranu A ");
+string stranaA = Console.ReadLine();
+
+Console.WriteLine("Zadej stranu B ");
+string stranaB = Console.ReadLine();
+
+Console.WriteLine("Zadej stranu C: ");   
+string stranaC = Console.ReadLine();
+
+
+Console.WriteLine("Zadej vysku: ");   
+string vyska = Console.ReadLine();
+
+
+
+int výsledek = Convert.ToInt32(stranaA) * Convert.ToInt32(stranaB) * Convert.ToInt32(vyska);
+int výsledek2 = Convert.ToInt32(stranaA) * Convert.ToInt32(stranaB) * Convert.ToInt32(stranaC);
+
+Console.WriteLine("Výsledek obsah lichoběžníku je: " + výsledek);
+Console.WriteLine("Výsledek Povrchu Kvádru je: " + výsledek2);
--- obj/Debug/net9.0/NazevProjektu.AssemblyInfo.cs
+++ obj/Debug/net9.0/NazevProjektu.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NazevProjektu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+830a19ad276748c7f7a62ad9e942c577dc6568f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("NazevProjektu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NazevProjektu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
